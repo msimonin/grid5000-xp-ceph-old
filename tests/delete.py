c931af4252432b2778a58f090d2250b9f46e512c
msimonin <matthieu.simonin@inria.fr>
2014-10-13 23:09:36 +0200

    add delete script.
    M

---
+++ tests/delete.py
@@ -0,0 +1,32 @@
+import boto
+import boto.s3.connection
+access_key = '12345'
+secret_key = '12345'
+
+conn = boto.connect_s3(
+        aws_access_key_id = access_key,
+        aws_secret_access_key = secret_key,
+        host = 'localhost',
+        is_secure=False,               # uncommmnt if you are not using ssl
+        calling_format = boto.s3.connection.OrdinaryCallingFormat(),
+)
+
+
+print "Deleting all buckets"
+
+for bucket in conn.get_all_buckets():
+        print "{name}\t{created}".format(
+                name = bucket.name,
+                created = bucket.creation_date,
+        )
+	for key in bucket.list():
+        	print "\t{name}\t{size}\t{modified}".format(
+                name = key.name,
+                size = key.size,
+                modified = key.last_modified,
+                )
+		key.delete()
+	bucket.delete()
+
+print "done"
+
